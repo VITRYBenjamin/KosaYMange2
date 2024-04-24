be98a5103ff2346be8f97dc9da1420c94665eda6
VITRYBenjamin <vitry.benjamin11@gmail.com>
2024-04-24 10:11:04 +0400

    Ajout : Activity2 - Activité principale après la connexion.
    Fonctionalité :
    Passage d'une activité à l'autre.

--- app/src/main/java/com/example/kosaymange/Activity2.kt
+++ app/src/main/java/com/example/kosaymange/Activity2.kt
@@ -1,29 +1,20 @@
 package com.example.kosaymange
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnConnexion = findViewById<Button>(R.id.buttonConnexion)
-
-        btnConnexion.setOnClickListener {
-            val intent = Intent(this, Activity2::class.java)
-            startActivity(intent)
         }
     }
 }
