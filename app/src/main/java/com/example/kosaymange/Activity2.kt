570e2400fce782c300a2cd26be544bd4a15843ce
VITRYBenjamin <vitry.benjamin11@gmail.com>
2024-04-26 09:29:04 +0400

    Fonctionalité : Menu de navigation fonctionnel.

--- app/src/main/java/com/example/kosaymange/Activity2.kt
+++ app/src/main/java/com/example/kosaymange/Activity2.kt
@@ -1,20 +1,58 @@
 package com.example.kosaymange
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class Activity2 : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> {
+                    Toast.makeText(applicationContext, "Home Clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.nav_profile -> {
+                    Toast.makeText(applicationContext, "Home Clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.nav_week -> {
+                    Toast.makeText(applicationContext, "Home Clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.nav_meals -> {
+                    Toast.makeText(applicationContext, "Home Clicked", Toast.LENGTH_SHORT).show()
+                }
+                R.id.nav_ingredients -> {
+                    Toast.makeText(applicationContext, "Home Clicked", Toast.LENGTH_SHORT).show()
+                }
+            }
+            drawerLayout.close()
+            true
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
